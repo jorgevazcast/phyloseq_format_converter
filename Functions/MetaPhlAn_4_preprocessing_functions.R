646352cdfe71a74cab1b6f24426b0c8a08133def
u0141268 <jorgefrancisco.vazquezcastellanos@kuleuven.be>
2025-02-17 10:23:44 +0100

    Initial commit

---
+++ Functions/MetaPhlAn_4_preprocessing_functions.R
@@ -0,0 +1,183 @@
+set.seed(12345)
+library(phyloseq)
+
+###################################################################
+########################     Functions     ########################
+rar_phylo <- function(phylo.obj,Nreads){
+	cat("N reads: ", Nreads, "\n")
+	phylo.obj.rar = rarefy_even_depth( phylo.obj , replace=FALSE,  sample.size = Nreads )
+	taxSums <- taxa_sums(phylo.obj.rar)
+	phylo.obj.rar <- prune_taxa(names(taxSums[taxSums > 0]),phylo.obj.rar)
+
+	Otable <- data.frame(t(otu_table(phylo.obj.rar)))
+	(raremax <- min(rowSums(Otable)))	
+	pdf( paste0("rarecurve.VDP.mOTUs.",Nreads,".pdf"),width=10)
+	rarecurve(Otable, step = 20, sample = raremax, col = "blue", cex = 0.6, label = F)
+	dev.off()
+	
+	return(phylo.obj.rar)
+	
+}
+
+read_infile_MetaPhlAn <- function(in.file = "", tax_level = ""){  # Levels: SGB, species, genus
+
+	Taxa_df <- utils::read.delim(in.file, sep = "\t", check.names = F)
+	Taxa_df$clade_name <- as.character(Taxa_df$clade_name)
+
+	if (tax_level == "SGB") {
+		Taxa_df <- Taxa_df[grepl("t__",Taxa_df$clade_name),]
+		Select.Taxa.level <- sapply(Taxa_df$clade_name, function(x){
+				vect <- unlist(strsplit(x, "[|]"))
+				TaxLevel <- grepl("t__",vect[length(vect)])	
+				return(TaxLevel)})	
+	}		
+	if (tax_level == "species") {
+		Taxa_df <- Taxa_df[grepl(";s__",Taxa_df$clade_name),]
+		Select.Taxa.level <- sapply(Taxa_df$clade_name, function(x){
+				vect <- unlist(strsplit(x, "[|]"))
+				TaxLevel <- grepl("s__",vect[length(vect)])	
+				return(TaxLevel)})	
+	}
+	if (tax_level == "genus") {
+		Taxa_df <- Taxa_df[grepl(";g__",Taxa_df$clade_name),]
+		Select.Taxa.level <- sapply(Taxa_df$clade_name, function(x){
+				vect <- unlist(strsplit(x, "[|]"))
+				TaxLevel <- grepl("g__",vect[length(vect)])	
+				return(TaxLevel)})			
+	}
+	
+	row.names(Taxa_df) = Taxa_df$clade_name
+	print(all(names(Select.Taxa.level) == rownames(Taxa_df)))
+	Taxa_df <- Taxa_df[ Select.Taxa.level == T, ]
+	Taxa_df$clade_name = NULL
+	
+	Taxa_df = Taxa_df[, colSums(Taxa_df) != 0]
+	Taxa_df = Taxa_df[rowSums(Taxa_df) != 0, ]
+	return(Taxa_df)
+}
+
+read_infile_MetaPhlAn_GTDB <- function(in.file = "", tax_level = ""){  # Levels:  species, genus
+
+	Taxa_df <- utils::read.delim(in.file, sep = "\t", check.names = F)
+	Taxa_df$clade_name <- as.character(Taxa_df$clade_name)
+
+		
+	if (tax_level == "species") {
+		Taxa_df <- Taxa_df[grepl(";s__",Taxa_df$clade_name),]
+		Select.Taxa.level <- sapply(Taxa_df$clade_name, function(x){
+				vect <- unlist(strsplit(x, ";"))
+				TaxLevel <- grepl("^s__",vect[length(vect)])	
+				return(TaxLevel)})	
+	}
+	if (tax_level == "genus") {
+		Taxa_df <- Taxa_df[grepl(";g__",Taxa_df$clade_name),]
+		Select.Taxa.level <- sapply(Taxa_df$clade_name, function(x){
+				vect <- unlist(strsplit(x, ";"))
+				TaxLevel <- grepl("^g__",vect[length(vect)])	
+				return(TaxLevel)})			
+	}
+	
+	row.names(Taxa_df) = Taxa_df$clade_name
+	print(all(names(Select.Taxa.level) == rownames(Taxa_df)))
+	Taxa_df <- Taxa_df[ Select.Taxa.level == T, ]
+	Taxa_df$clade_name = NULL
+	
+	Taxa_df = Taxa_df[, colSums(Taxa_df) != 0]
+	Taxa_df = Taxa_df[rowSums(Taxa_df) != 0, ]
+	return(Taxa_df)
+}
+
+MetaPhlAn_names_databases <- function(namesTaxa = c(), DB=c("mpa","GTDB_r207")  ){
+
+	taxa_names <- namesTaxa
+	if(DB == "mpa"){
+		### Convert the MetaPhlAn taxonomy to the one that of the dada2 format
+		taxa_names <- gsub("k__","k_", taxa_names)
+		taxa_names <- gsub("p__","p_", taxa_names)
+		taxa_names <- gsub("c__","c_", taxa_names)
+		taxa_names <- gsub("o__","o_", taxa_names)	
+		taxa_names <- gsub("f__","f_", taxa_names)			
+		taxa_names <- gsub("g__","g_", taxa_names)			
+		taxa_names <- gsub("s__","s_", taxa_names)			
+		taxa_names <- gsub("t__","t_", taxa_names)					
+	}
+	if(DB == "GTDB_r207"){
+		### Convert the MetaPhlAn taxonomy to the one that of the dada2 format
+		taxa_names <- gsub("d__","k_", taxa_names)
+		taxa_names <- gsub("p__","p_", taxa_names)
+		taxa_names <- gsub("c__","c_", taxa_names)
+		taxa_names <- gsub("o__","o_", taxa_names)	
+		taxa_names <- gsub("f__","f_", taxa_names)			
+		taxa_names <- gsub("g__","g_", taxa_names)			
+		taxa_names <- gsub("s__","s_", taxa_names)			
+		taxa_names <- gsub("t__","t_", taxa_names)					
+	}
+	return(taxa_names)
+}
+
+phyloseq_format_MetaPhlAn <- function(in.data, database=c("mpa","GTDB_r207")){
+
+	cat("\nCheck that the taxa is in the rownames\nrownames: \n", head(rownames(in.data)),"\n\n")
+	cat("\nDB taxonomy: ", database,"\n\n")
+
+	taxa_names <- MetaPhlAn_names_databases( namesTaxa= rownames(in.data), DB=database )
+	
+	rownames(in.data) <- taxa_names
+
+	if(database == "mpa"){ split_data <- stringr::str_split(rownames(in.data), "\\|") }
+	if(database == "GTDB_r207"){ split_data <- stringr::str_split(rownames(in.data), ";") }
+		
+	max_length <- max(lengths(split_data))
+
+	# Fill in missing elements with NA
+	filled_data <- lapply(split_data, function(x) c(x, rep(NA, max_length - length(x))))
+	
+	# Convert the list to a data frame
+	Tax_df <- as.data.frame(do.call(rbind, filled_data))	
+	rownames(Tax_df) <- taxa_names	
+
+	Tax_df[Tax_df == "t_"] <- NA
+	Tax_df[Tax_df == "g_"] <- NA
+	Tax_df[Tax_df == "f_"] <- NA
+	Tax_df[Tax_df == "o_"] <- NA
+	Tax_df[Tax_df == "c_"] <- NA
+	Tax_df[Tax_df == "p_"] <- NA
+	Tax_df[Tax_df == "k_"] <- NA			
+					
+	### Complete the NA taxonomy 
+	for( i in 1:nrow(Tax_df) ){
+		# i <- 927
+		temp.line <- as.character(Tax_df[i,])
+		if( any(is.na(temp.line)) ){
+			
+			na.ind  <- is.na(temp.line)			
+			na.lev <- min( grep("TRUE",as.character( na.ind )) ) ### Locates the first NA taxonomic label
+
+			if(na.lev == 1 & temp.line[na.lev] == "unassigned"){
+				Tax_df[i,] <- "unassigned"
+				
+			}else{
+				### Takes the last-known taxonomic category
+				last.tax <- tail(as.character(temp.line[!na.ind]),n=1)
+				 ### Add an uc_LAST_KNOWN_TAX_LEVEL to all the unkwon taxonomic levels, ie: 
+				#temp.line[,(na.lev+1):length(temp.line)] <- paste0("uc_",last.tax)
+				temp.line[is.na(temp.line)] <- paste0("uc_",last.tax)
+				### Replace the taxonomy
+				#print(temp.line)
+				Tax_df[i,]<-temp.line
+			}	
+		}
+	}
+
+	TaxColumns<- c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species","SGB")		
+	
+	colnames(Tax_df) <- TaxColumns[1:length(colnames(Tax_df))]
+	
+	
+	OTUs <- phyloseq::otu_table(in.data,taxa_are_rows=T)
+	TAXA <- phyloseq::tax_table(as(Tax_df,"matrix"))
+	
+	out.phylo <- phyloseq::phyloseq(OTUs,TAXA)
+	return(out.phylo)
+}
+
